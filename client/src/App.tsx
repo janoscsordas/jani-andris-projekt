f213e509220a36a471cadbae91b7cd08d89b2227
AndrisVasari <andrasvasari47@gmail.com>
2024-10-09 13:14:10 +0200

    Added some libraries and built the login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,10 @@
-
-import { useState } from "react"
-
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import tailwindLogo from "./assets/tailwind.svg"
+import Login from "./components/Login.tsx"
 
 export default function App() {
-    const [count, setCount] = useState(0)
 
     return (
-        <main className="w-full h-screen flex flex-col justify-center items-center bg-[#1e1e20]">
-            <div className="flex gap-16 items-center mb-16">
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo w-28 h-28" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react w-28 h-28" alt="React logo" />
-                </a>
-                <a href="https://tailwindcss.com" target="_blank">
-                    <img src={tailwindLogo} className="logo tailwind w-28 h-28" alt="TailwindCSS logo" />
-                </a>
-            </div>
-            <h1 className="text-4xl font-mono font-semibold text-gray-300">Vite + React + TailwindCSS</h1>
-            <p className="text-gray-300 mt-8">This package was made by <a href="https://github.com/janoscsordas" target="_blank" className="text-[#747bff] hover:underline">Janos Csordas</a></p>
-            <div className="mt-8">
-                <button className="block mx-auto font-mono bg-zinc-800 text-gray-300 rounded-lg px-5 py-3 mb-6 border border-transparent hover:border-[#747bff] transition-colors duration-200" onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-                </button>
-                <p className="text-white mb-2">
-                Edit <code className="text-[#747bff]">src/App.tsx</code> and save to test HMR (Hot Module Reload)
-                </p>
-            </div>
-            <p className="text-white">
-                Click on the Vite, React and TailwindCSS logos to learn more
-            </p>
+        <main className="font-mono">
+            <Login />
         </main>
     )
 }
---
+++ client/src/components/Login.tsx
@@ -0,0 +1,36 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
+import { EyeClosedIcon, EyeIcon } from "lucide-react"
+
+export default function Login() {
+    const [passwordIsSeen, setPasswordIsSeen] = useState(false)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handlePasswordVisibility = () => {
+        setPasswordIsSeen(!passwordIsSeen)
+    }
+
+    const handleLogin = (e: React.FormEvent) => {
+        e.preventDefault()
+        console.log({ email, password })
+    }
+
+    return (
+        <div className="w-full min-h-screen flex flex-col justify-center items-center bg-background">
+            <h1 className="text-3xl font-bold text-white text-center mb-1">Bejelentkezés a fiókba</h1>
+            <p className="text-gray-400 text-sm mb-4">A belépéshez írja be az email címét és a jelszavát</p>
+            <form action="" onSubmit={handleLogin}>
+                <Input className="w-[20rem] mb-2" type="email" placeholder="Email cím" required={true} value={email} onChange={(e) => setEmail(e.target.value)} />
+                <div className="relative">
+                    <Input type={passwordIsSeen ? "text" : "password"} placeholder="Jelszó" required={true} maxLength={24} value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <span onClick={handlePasswordVisibility} className="z-50 absolute right-2 top-1.5 cursor-pointer text-gray-300">
+                        {passwordIsSeen ? <EyeClosedIcon /> : <EyeIcon />}
+                    </span>
+                </div>
+                <Button type="submit" className="w-[20rem] mt-2">Bejelentkezés</Button>
+            </form>
+        </div>
+    )
+}
